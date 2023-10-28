ce12b341b486e33c7f3485c3ba32b2db5a55e74c
Icaro-Lima <icarodantas.lima@gmail.com>
2023-10-28 17:17:41 -0300

    Working simulator

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { CssBaseline } from '@mui/material';
 export const TranspilationContext = React.createContext({
   isTranspiling: false,
   logs: new Array<Log>(),
+  transpiledCode: '',
   onRunClick: (_code: string) => {},
   onClearLogsClick: () => {},
 });
@@ -17,12 +18,14 @@ export const TranspilationContext = React.createContext({
 function App() {
   const [isTranspiling, setIsTranspiling] = React.useState(false);
   const [logs, setLogs] = React.useState<Log[]>([]);
+  const [transpiledCode, setTranspiledCode] = React.useState('');
 
   const onRunClick = (code: string) => {
     setIsTranspiling(true);
+    setTranspiledCode('');
 
     Transpiler.transpile(code, (log) => setLogs((logs) => [...logs, log]))
-      .then()
+      .then((transpiledCode) => setTranspiledCode(transpiledCode))
       .catch(() => {})
       .finally(() => setIsTranspiling(false));
   };
@@ -35,10 +38,11 @@ function App() {
     return {
       isTranspiling,
       logs,
+      transpiledCode,
       onRunClick,
       onClearLogsClick,
     };
-  }, [isTranspiling, logs]);
+  }, [isTranspiling, logs, transpiledCode]);
 
   return (
     <ToggleColorMode>
--- src/simulator/Simulator.tsx
+++ src/simulator/Simulator.tsx
@@ -1,17 +1,58 @@
 import React from 'react';
 import MainBoard from './MainBoard';
+import { TranspilationContext } from '../App';
 
 interface SimulatorProps {}
 
 function Simulator(props: SimulatorProps) {
-  const [value, setValue] = React.useState(0);
+  const [seg, setSeg] = React.useState(0);
+  const [swi, setSwi] = React.useState(0);
+  const [led, setLed] = React.useState(0);
+  const [clk, setClk] = React.useState(false);
+
+  const transpilationContext = React.useContext(TranspilationContext);
+
+  const module = React.useMemo(() => {
+    if (transpilationContext.transpiledCode === '') {
+      return null;
+    }
+
+    // eslint-disable-next-line no-new-func
+    return new Function(
+      transpilationContext.transpiledCode + '\n\nreturn Module;',
+    )();
+  }, [transpilationContext.transpiledCode]);
+
+  const trySimulate = React.useCallback(() => {
+    if (module === null || module.simulate === undefined) {
+      return;
+    }
+
+    const simulation = JSON.parse(module.simulate(swi, clk));
+    setLed(simulation.led);
+    setSeg(simulation.seg);
+  }, [clk, module, swi]);
+
+  const onSwiChange = (swi: number) => {
+    setSwi(swi);
+
+    trySimulate();
+  };
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setClk(!clk);
+    }, 1000);
+
+    trySimulate();
+  }, [clk, trySimulate]);
 
   return (
     <MainBoard
-      sevenSegmentsDisplayValue={value}
-      switchesDefaultValue={value}
-      ledsValue={value}
-      onSwitchesChange={(value) => setValue(value)}
+      sevenSegmentsDisplayValue={seg}
+      switchesDefaultValue={swi}
+      ledsValue={led}
+      onSwitchesChange={onSwiChange}
     />
   );
 }
